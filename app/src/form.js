62c2804b9d0f8f38b09771ff01d45c33d0caf0c4
Mitesh <miteshkumarca@gmail.com>
2020-09-26 09:37:16 -0500

    added twitter_embed and email

--- app/src/form.js
+++ app/src/form.js
@@ -15,6 +15,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Form(props) {
   const [review, setReview] = useState("");
+  const [email, setEmail] = useState(""); 
   const classes = useStyles(); 
   
   const handleSubmit = (evt) => {
@@ -26,11 +27,13 @@ export default function Form(props) {
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({description: review })
+        body: JSON.stringify({description: review + "   -   " + email })
     };
+    
     fetch('http://localhost:8080/api/reviews', requestOptions)
         .then(response => response.json());
     setReview(""); 
+    setEmail(""); 
   }
 
   return (
@@ -38,6 +41,7 @@ export default function Form(props) {
     <form onSubmit={handleSubmit}>
         
       <TextField id="filled-basic" label="Tell us about your experience..." variant="filled" value={review} onChange={e => setReview(e.target.value) } />
+      <TextField id="filled-basic" label="PQC Email" variant="filled" value={email} onChange={e => setEmail(e.target.value) } />
       <input type="submit" value="Submit" />
     </form>
     </div>
