c3d970742d3f22d4d54b0c22810b9366e92abc13
Mitesh <miteshkumarca@gmail.com>
2020-09-25 23:35:58 -0500

    fixed node modules

--- app/src/WC.js
+++ app/src/WC.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import ReactWordCloud from 'react-wordcloud'; 
 
-const words = [
-    {
-        text: 'told',
-        value: 64,
-    },
-    {
-        text: 'mistake',
-        value: 11,
-    },
-    {
-        text: 'thought',
-        value: 16,
-    },
-    {
-        text: 'bad',
-        value: 17,
-    },
-]; 
+// const words = [
+//     {
+//         text: 'told',
+//         value: 64,
+//     },
+//     {
+//         text: 'mistake',
+//         value: 11,
+//     },
+//     {
+//         text: 'thought',
+//         value: 16,
+//     },
+//     {
+//         text: 'bad',
+//         value: 17,
+//     },
+// ]; 
 
-export default function WC () {
-    return <ReactWordCloud words={words} />
+
+class Reviews extends React.Component {
+    componentWillMount() {
+        fetch('./data.csv')
+        .then((r) => r.text())
+        .then(text => {
+                let tmp = text.split("\n").map((entry, idx) => {
+                let A = tmp.split(","); 
+                return {
+                    "message": A[0],
+                    "stars": int(A[1]),
+                };
+            });
+
+        });
+    }
+}
+export default function WC (pass_words) {
+    return <ReactWordCloud words={pass_words} />
 }
 
